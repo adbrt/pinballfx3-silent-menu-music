f0963e463eb9f2fc39daf06d2a3fce4836fa3766
adbrt <88463212+adbrt@users.noreply.github.com>
2022-08-23 15:35:25 +0200

    Update gui_sfx_silencer.bat

--- gui_sfx_silencer.bat
+++ gui_sfx_silencer.bat
@@ -34,7 +34,7 @@ set TGT_FILE6=music_upgrade.ogg
 
 FOR /L %%i IN (1,1,6) DO (
 echo.
-echo Trying to remap !TGT_FILE! to %SRC_FILE%
+echo Trying to remap !TGT_FILE%%i! to %SRC_FILE%
 echo. 
 powershell "$source = '%SRC_FILE%' ; $target = '!TGT_FILE%%i!' ; $data = (Get-Content -path %PXP_FILE% -Raw); $bytes  = [System.IO.File]::ReadAllBytes('%PXP_FILE%'); $nm = $data.IndexOf($source) ; if ($nm -lt 1) {echo \""source not found, quitting\""; exit 1}; echo \""found $source at offset $nm\"" ; $m1 = $data.IndexOf($target) ; if ($m1 -lt 1) {echo \""target not found, quitting\""; exit 1} ; echo \""found $target at offset $m1\"" ; $bytes[$m1-2] = $bytes[$nm-2]; $bytes[$m1-3] = $bytes[$nm-3] ; $bytes[$m1-4] = $bytes[$nm-4]; $bytes[$m1-5] = $bytes[$nm-5] ; $bytes[$m1-6] = $bytes[$nm-6]; $bytes[$m1-7] = $bytes[$nm-7] ; $bytes[$m1-8] = $bytes[$nm-8]; $bytes[$m1-9] = $bytes[$nm-9] ; $bytes[$m1-10] = $bytes[$nm-10]; $bytes[$m1-11] = $bytes[$nm-11] ; $bytes[$m1-12] = $bytes[$nm-12] ; $bytes[$m1-13] = $bytes[$nm-13] ; $bytes[$m1-14] = $bytes[$nm-14] ; $bytes[$m1-15] = $bytes[$nm-15]  ; [System.IO.File]::WriteAllBytes('%PXP_FILE%', $bytes); echo \""Remapped\""; echo ''"
 
